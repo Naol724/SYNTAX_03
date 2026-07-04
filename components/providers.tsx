029f0e0cccc2fea068eccdbb4752bd79fff3db2d
Naol Gonfa <naolgonfa449@gmail.com>
2026-07-05 02:27:08 +0300

    provider file inside component file updated

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,15 +3,18 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
+import { SessionProvider } from "next-auth/react";
 import { queryClient } from "@/lib/queryClient";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        {children}
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          {children}
+          <Toaster />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
